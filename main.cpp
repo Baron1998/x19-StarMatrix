cca0797b9cae3393da33658038a4b2f375f7debe
Baron1998 <abaron3@mail.csuchico.edu.com>
2017-10-18 16:56:54 -0700

    row and columns of star

---
+++ main.cpp
@@ -0,0 +1,51 @@
+//Author : Abdullah Baron
+
+#include<string>
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{ 
+  vector <vector <char> > star;
+  int rowNumber;
+  int colNumber;
+  
+  cout<< "Enter rows of stars\n";
+  cin>> rowNumber;
+  cout<<" Enter columns of stars\n";
+  cin>> colNumber;
+  star.resize(rowNumber);
+if (rowNumber >0 && colNumber>0 )
+  {
+
+    
+  for( int row= 0; row<star.size();row++)
+    {
+     star[row].resize(colNumber);
+      for( int col=0 ;col<star[row].size();col++)
+       {
+        star[row][col] = '*';
+     
+        cout<< star[row][col];
+       
+       
+       }
+        cout<< endl;
+
+    }
+  }      
+
+
+
+
+
+
+
+
+
+
+return(0);
+}
+
